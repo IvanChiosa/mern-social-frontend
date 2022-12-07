970d61fde229edca4abd5682f36afbddd1b425ea
Ivan <i85chiosa@gmail.com>
2022-12-07 09:23:31 +0100

    Name geendert

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
         textAlign="center"
       >
         <Typography fontWeight="bold" fontSize="32px" color="primary">
-          Sociopedia IB
+        SocialLife ✨
         </Typography>
       </Box>
 
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Socipedia, the Social Media IB!
+          Welcome to SocialLife! 🤪
         </Typography>
         <Form />
       </Box>
--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia IB
+          SocialLife ✨
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
@@ -158,9 +158,9 @@ const Navbar = () => {
                 <LightMode sx={{ color: dark, fontSize: "25px" }} />
               )}
             </IconButton>
-            <Message sx={{ fontSize: "25px" }} />
+            {/* <Message sx={{ fontSize: "25px" }} />
             <Notifications sx={{ fontSize: "25px" }} />
-            <Help sx={{ fontSize: "25px" }} />
+            <Help sx={{ fontSize: "25px" }} /> */}
             <FormControl variant="standard" value={fullName}>
               <Select
                 value={fullName}
